57c9796a39050c0ac58fda4fe34d2458421192d6
Jacob Dachenhaus <dachendev@gmail.com>
2023-08-06 22:04:46 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  args.forEach((value) => {
+    let index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  });
 
+  return arr;
 };
 
 // Do not edit below this line

81c019afcca0fabe15e4e1e5cccca09f96c31915
Jacob Dachenhaus <dachendev@gmail.com>
2023-08-06 22:45:14 -0400

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+function round(num, precision = 0) {
+  let multi = Math.pow(10, precision);
+  return Math.round(num * multi) / multi;
+}
+
+const convertToCelsius = function (tempF) {
+  return round((tempF - 32) * (5 / 9), 1);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  return round(tempC * (9 / 5) + 32, 1);
 };
 
 // Do not edit below this line
